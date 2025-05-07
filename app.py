be2cbe8d3d9b4c739ec1bdbd8b1f1095e7794449
Denys Sikora <sikora.denys@chnu.edu.ua>
2025-05-07 21:31:17 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# --- Параметри підключення до MongoDB ---
+uri = "mongodb+srv://sikora:20050411@db1.gyaftca.mongodb.net/?retryWrites=true&w=majority&appName=DB1"
+client = MongoClient(uri)
+db = client.mydatabase  # Замініть на назву вашої бази даних
+collection = db.mycollection  # Замініть на назву вашої колекції
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        age_str = request.form['age']
+        if name and age_str.isdigit():
+            age = int(age_str)
+            data = {"name": name, "age": age}
+            try:
+                result = collection.insert_one(data)
+                return redirect(url_for('index'))
+            except Exception as e:
+                return f"Помилка вставки: {e}"
+    # Отримання всіх документів з MongoDB
+    documents = collection.find()
+    return render_template('index.html', documents=documents)
+
+if __name__ == "__main__":
+    app.run(debug=True)
